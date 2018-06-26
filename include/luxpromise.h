cc97f8a3dfdb1e421b7dfb0bcc4cf705826cbc79
fmitchell <fmitchell@luxoft.com>
2018-06-26 15:19:38 +0200

    in progress of enhancing the LuxPromise class

--- include/luxpromise.h
+++ include/luxpromise.h
@@ -6,67 +6,78 @@
  */
 
 namespace lux {
-enum class Types { property, method, event };
+  /**
+   * property     -- property-type promise, optional blocking
+   * method       -- method-type promise, always block, parameters passed in aggeration
+   * event        -- event-type promise, always blocks
+   * notification -- master notifier that one of the above has changed or triggered
+   */
+  enum class Types { property, method, event, notification };
 
-using mutex_l = std::mutex;
-using unique_l = std::unique_lock<mutex_l>;
-using condvar = std::condition_variable;
+  using mutex_l = std::mutex;
+  using unique_l = std::unique_lock<mutex_l>;
+  using condvar = std::condition_variable;
 
-/**
- */
-template <typename Data, Types LT = Types::property> class Promise {
+  /**
+   */
+  template <typename Data, Types LType = Types::property> class Promise {
 
-  Data data_;
-  mutex_l mutex_;
-  condvar cv_;
-  bool fresh_ = false;
-  bool active_ = true;
+    Data data_;
+    mutex_l mutex_;
+    condvar cv_;
+    bool fresh_ = false;
+    bool active_ = true;
 
-  template <typename LOCK>
-  void wakeUp(LOCK &lk) {
-    fresh_ = true;
-    lk.unlock();
-    cv_.notify_all();
-  }
+    template <typename LOCK> void wakeUp(LOCK &lk) {
+      fresh_ = true;
+      lk.unlock();
+      cv_.notify_all();
+    }
 
-  template <typename LOCK>
-  inline void raw_wait(LOCK& lk, bool wait_for = true) {
-    cv_.wait(lk, [&] { return !active_ || (fresh_ || !wait_for); });
-  }
+    template <typename LOCK>
+    inline void raw_wait(LOCK &lk, bool wait_for = true) {
+      switch (LType) {
+      case Types::method:
+      case Types::event:
+      case Types::notification:
+        wait_for = true; // force the wait regardless
+      }
+      cv_.wait(lk, [&] { return !active_ || (fresh_ || !wait_for); });
+    }
 
-public:
-  Promise(Data data) : data_(data) {}
+  public:
+    Promise(Data data) : data_(data) {}
 
-  Promise &operator=(Data &&update_data) {
-    unique_l lk(mutex_);
-    data_ = std::move(update_data);
-    wakeUp(lk);
-    return *this;
-  }
+    Promise &operator=(Data &&update_data) {
+      unique_l lk(mutex_);
+      data_ = std::move(update_data);
+      wakeUp(lk);
+      return *this;
+    }
 
-  Promise &operator=(Data &update_data) {
-    unique_l lk(mutex_);
-    data_ = update_data;
-    wakeUp(lk);
-    return *this;
-  }
+    Promise &operator=(Data &update_data) {
+      unique_l lk(mutex_);
+      data_ = update_data;
+      wakeUp(lk);
+      return *this;
+    }
 
-  /// Will block if active not fresh
-  const Data &operator()(bool wait_for = true) {
-    unique_l lk(mutex_);
-    raw_wait(lk, wait_for);
-    fresh_ = false;
-    return data_;
-  }
+    /// Will block if active not fresh
+    const Data &operator()(bool wait_for = true) {
+      unique_l lk(mutex_);
+      raw_wait(lk, wait_for);
+      fresh_ = false;
+      return data_;
+    }
 
-  inline void end_updates() { active_ = false; }
-  inline bool is_active() { return active_; }
+    inline void end_updates() { active_ = false; }
+    inline bool is_active() { return active_; }
 
-  /// Will wait while staying fresh.
-  inline Promise<Data, LT> &wait_for_update() {
-    unique_l lk(mutex_);
-    raw_wait(lk);
-    return *this;
-  }
-};
+    /// Will wait while staying fresh.
+    inline Promise<Data, LType> &wait_for_update() {
+      unique_l lk(mutex_);
+      raw_wait(lk);
+      return *this;
+    }
+  };
 }
