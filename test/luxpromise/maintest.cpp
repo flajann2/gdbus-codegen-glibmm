f1f93d0d3cf3b1626f58883a8cfbf27208055139
fmitchell <fmitchell@luxoft.com>
2018-06-26 15:33:55 +0200

    testing for method

--- test/luxpromise/maintest.cpp
+++ test/luxpromise/maintest.cpp
@@ -7,19 +7,22 @@
 constexpr int countdown = 28000;
 
 namespace lux {
-class PromiseTests : public ::testing::Test {
-protected:
-  Promise<int> data{countdown};
+  class PromiseTests : public ::testing::Test {
+  protected:
+    Promise<int, Types::property> data{countdown};
+    Promise<int, Types::method> method{countdown};
+    Promise<int, Types::event> event{countdown};
+    Promise<int, Types::notification> notif{countdown};
 
-  void SetUp() override {}
+    void SetUp() override {}
 
-  virtual void TearDown() {}
-};
+    virtual void TearDown() {}
+  };
 }
 
 using namespace lux;
 
-TEST_F(PromiseTests, test_basic_functionality) {
+TEST_F(PromiseTests, test_property_mode) {
 
   std::thread t1([&]() {
     for (auto i = countdown; i > 0; --i) {
@@ -38,6 +41,29 @@ TEST_F(PromiseTests, test_basic_functionality) {
     }
     last_data_snapshot = data_snapshot;
     data.wait_for_update();
+  }
+
+  t1.join();
+}
+
+TEST_F(PromiseTests, test_method_mode) {
+
+  std::thread t1([&]() {
+    for (auto i = countdown; i > 0; --i) {
+      std::this_thread::sleep_for(std::chrono::microseconds(1));
+      method = i;
+    }
+    method.end_updates();
+  });
+
+  int meth_snapshot;
+  int last_meth_snapshot = data() + 1;
+
+  while ((meth_snapshot = method()) > 1) {
+    if (method.is_active()) {
+      ASSERT_TRUE(meth_snapshot < last_meth_snapshot);
+    }
+    last_meth_snapshot = meth_snapshot;
   }
 
   t1.join();
