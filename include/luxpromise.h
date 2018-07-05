3d6ac989ba1569cf9ef6d5b6a54d580650d438bb
fmitchell <fmitchell@luxoft.com>
2018-07-05 17:25:18 +0200

    semantics in place

--- include/luxpromise.h
+++ include/luxpromise.h
@@ -1,7 +1,7 @@
 #include <condition_variable>
+#include <iostream> // TODO: for debugging. remove this line.
 #include <mutex>
 #include <thread>
-#include <iostream> // TODO: for debugging. remove this line.
 
 /** Please see the README.org for the details of the
  * locking / waiting semantics.
@@ -16,10 +16,7 @@ namespace lux {
    * notification -- master notifier that one of the above has changed or
    * triggered
    */
-  enum class ptype { property,
-                     method,
-                     event,
-                     notification };
+  enum class ptype { property, method, event, notification };
 
   using mutex_l = std::mutex;
   using unique_l = std::unique_lock<mutex_l>;
@@ -36,39 +33,42 @@ namespace lux {
     bool fresh_ = false;
     bool active_ = true;
 
+    /// Called by reads
     void stale_wake_up(unique_l &lk) {
-      fresh_ = false;
+      if (LType != ptype::property) {
+        fresh_ = false;
+      }
       lk.unlock();
       cv_.notify_all();
     }
 
+    /// Called by writes
     void fresh_wake_up(unique_l &lk) {
       fresh_ = true;
       lk.unlock();
       cv_.notify_all();
     }
 
-    inline void wait_for_fresh(unique_l &lk, bool wait_for = true) {
-      //cout << "wait_for_fresh("  << (int) LType << "): active = " << active_ << " fresh " << fresh_ << " wait_for " << wait_for << endl;
-      switch (LType) {
-      case ptype::method:
-      case ptype::event:
-      case ptype::notification:
-        wait_for = true; // force the wait regardless
-      }
-      cv_.wait(lk, [&] { return !active_ || (fresh_ || !wait_for); });
+    /// This is called on reads
+    inline void wait_for_fresh(unique_l &lk) {
+      // cout << "wait_for_fresh("  << (int) LType << "): active = " << active_
+      // << " fresh " << fresh_ << endl;
+      cv_.wait(lk, [&] { return !active_ || fresh_; });
     }
 
+    /// This is called for writes
     inline void wait_for_stale(unique_l &lk) {
-      //cout << "wait_for_stale(" << (int) LType << "): active = " << active_ << " fresh " << fresh_ << endl;
-      cv_.wait(lk, [&] { return !active_
-            || (LType == ptype::property) || (LType == ptype::notification)
-            || !fresh_; });
+      // cout << "wait_for_stale(" << (int) LType << "): active = " << active_
+      // << " fresh " << fresh_ << endl;
+      cv_.wait(lk, [&] {
+        return !active_ || (LType == ptype::property) ||
+               (LType == ptype::notification) || !fresh_;
+      });
     }
 
   public:
     explicit promise() = default;
-    promise(const promise&) = delete;
+    promise(const promise &) = delete;
     explicit promise(Data data) : data_(data) {}
 
     promise &operator=(Data &&update_data) {
@@ -90,11 +90,11 @@ namespace lux {
     /// Will block if active not fresh
     /// Returns a copy of the data snapshot, though
     /// it is entirely possible the copy will be elided.
-    const Data operator()(bool wait_for = true) {
+    const Data operator()() {
       unique_l lk(mutex_);
-      wait_for_fresh(lk, wait_for);
+      wait_for_fresh(lk);
       auto snapshot_data = data_;
-    stale_wake_up(lk);
+      stale_wake_up(lk);
       return snapshot_data;
     }
 
